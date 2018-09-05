44ab5f7f51d7442961d5f6393f4056b26df67a44
James Johnson <jamesjohnson@Jamess-MacBook-Pro.local>
2018-09-05 17:25:38 -0400

    formatting changes

--- api/routes/fileupload.js
+++ api/routes/fileupload.js
@@ -1,58 +1,65 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const mongoose = require('mongoose');
-const formidable = require('formidable')
-const fs = require('fs');
+const mongoose = require("mongoose");
+const formidable = require("formidable");
+const fs = require("fs");
 
-const File = require('../models/fileModel');
+const File = require("../models/fileModel");
 
-const db = require('../../app');
+const db = require("../../app");
 
-router.get('/', (req, res, next) => {
-    File.find()
-        .exec()
-        .then(docs => {
-            console.log(docs);
-            res.status(200).json(docs);
-        }).catch(err => {
-            console.log(err);
-            res.status(500).json({
-                error: err
-            });
-        });
+router.get("/", (req, res, next) => {
+	File.find()
+		.exec()
+		.then(docs => {
+			console.log(docs);
+			res.status(200).json(docs);
+		})
+		.catch(err => {
+			console.log(err);
+			res.status(500).json({
+				error: err
+			});
+		});
 });
 
 router.post("/", (req, res, next) => {
-    const fileUpload = new File({
-        _id: new mongoose.Types.ObjectId(),
-        name: req.body.filetoupload
-    });
-    fileUpload.save().then(result => {
-        console.log(result);
-        res.status(201).json({
-            message: fileUpload.name + " was uploaded.",
-            id: fileUpload._id
-        });
-    }).catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error: err
-        });
-    });
+	const fileUpload = new File({
+		_id: new mongoose.Types.ObjectId(),
+		name: req.body.filetoupload
+	});
+	fileUpload
+		.save()
+		.then(result => {
+			console.log(result);
+			res.status(201).json({
+				message: fileUpload.name + " was uploaded.",
+				id: fileUpload._id
+			});
+		})
+		.catch(err => {
+			console.log(err);
+			res.status(500).json({
+				error: err
+			});
+		});
 });
 
-router.delete('/:fileID', (req, res, next) => {
-    const id = req.params.fileID;
-    File.deleteOne({
-        _id: id
-    }).exec().then(result => {
-        res.status(200).json(result);
-    }).catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error: err
-        });
-    });
+router.delete("/:fileID", (req, res, next) => {
+	const id = req.params.fileID;
+	File.remove({
+		_id: id
+	})
+		.exec()
+		.then(result => {
+			res.status(200).json(result);
+		})
+		.catch(err => {
+			console.log(err);
+			res.status(500).json({
+				error: err
+			});
+		});
 });
 
 module.exports = router;
