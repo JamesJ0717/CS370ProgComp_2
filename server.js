38a051f75e8e42700f9b511f389fa0c16dbd13f6
James Johnson <tripl3j547@gmail.com>
2018-09-19 17:18:36 -0400

    changed server port to 8080

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const http = require('http');
 const app = require('./app');
 
-const port = process.env.port || 80;
+const port = process.env.port || 8080;
 
 const server = http.createServer(app);
-console.log('Server listening on port 80');
+console.log('Server listening on port', port);
 server.listen(port);
