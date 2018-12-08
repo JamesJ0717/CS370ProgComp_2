d8eabe46f09d4eb3becb2f29f6b0c888419247c5
James Johnson <tripl3j547@gmail.com>
2018-12-08 14:24:31 -0500

    working on install script

--- install.sh
+++ install.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
-sudo apt-get update && sudo apt-get full-upgrade;
-sudo apt-get install nodejs docker npm;
-sudo npm i;
-sudo docker pull openjdk:alpine && sudo docker pull python:alpine;
+sudo su;
+apt-get update && apt-get full-upgrade -y;
+###NODE
+#get the latest version of node from nodeSource
+curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
+#install node.js
+apt-get install -y nodejs;
+###
+### DOCKER
+# get required dependancies for docker
+apt-get install apt-transport-https ca-certificates curl software-properties-common -y;
+# get gpg key for docker from docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -;
+apt-key fingerprint 0EBFCD88;
+#add the repository to the list of repositories
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable";
+#update and install
+apt update && apt install docker-ce;
+###
+apt-get install npm;
+npm i;
+docker pull openjdk:alpine &&  docker pull python:alpine;
