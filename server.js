cc8d0380cc58b2fd103bda3d40ae0b094dedfaed
James Johnson <tripl3j547@gmail.com>
2019-05-21 22:22:21 -0400

    Moving main node.js app to root folder

---
+++ server.js
@@ -0,0 +1,8 @@
+const http = require('http');
+const app = require('./app');
+
+const port = process.env.port || 9999;
+
+const server = http.createServer(app.app);
+console.log('Server listening on port', port);
+server.listen(port);
